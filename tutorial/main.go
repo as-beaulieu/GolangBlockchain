1de996900825cd2f04a5a3b2eb4cb0a26fe583e2
Andrew Beaulieu <as.beaulieu@gmail.com>
2020-10-25 16:52:35 -0600

    part 2 - refactor and proof of work

--- tutorial/main.go
+++ tutorial/main.go
@@ -1,55 +1,18 @@
 package main
 
 import (
-	"bytes"
-	"crypto/sha256"
+	"GolangBlockchain/tutorial/blockchain"
 	"fmt"
 )
 
-type BlockChain struct {
-	blocks []*Block
-}
-
-type Block struct {
-	Hash     []byte
-	Data     []byte
-	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
-}
-
-func CreateBlock(data string, prevHash []byte) *Block {
-	block := &Block{[]byte{}, []byte(data), prevHash}
-	block.DeriveHash()
-	return block
-}
-
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.blocks[len(chain.blocks)-1]
-	new := CreateBlock(data, prevBlock.Hash)
-	chain.blocks = append(chain.blocks, new)
-}
-
-func Genesis() *Block {
-	return CreateBlock("Genesis", []byte{})
-}
-
-func InitializeBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
-}
-
 func main() {
-	chain := InitializeBlockChain()
+	chain := blockchain.InitializeBlockChain()
 
 	chain.AddBlock("First block after genesis")
 	chain.AddBlock("Second block after genesis")
 	chain.AddBlock("Third block after genesis")
 
-	for i, block := range chain.blocks {
+	for i, block := range chain.Blocks {
 		fmt.Println("Block #", i)
 		fmt.Printf("previous hash: %x\n", block.PrevHash)
 		fmt.Printf("data in block: %s\n", block.Data)
