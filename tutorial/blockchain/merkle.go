67b1355da45d7f8f2cf3a1b2e28762825626cc57
Andrew Beaulieu <as.beaulieu@gmail.com>
2020-12-01 23:30:35 -0700

    Added NewMerkleNode()

--- tutorial/blockchain/merkle.go
+++ tutorial/blockchain/merkle.go
@@ -1,11 +1,31 @@
 package blockchain
 
+import "crypto/sha256"
+
 type MerkleTree struct {
 	RootNode *MerkleNode
 }
 
 type MerkleNode struct {
-	Left  *MerkleNode
+	Left  *MerkleNode //recursive
 	Right *MerkleNode
 	Data  []byte
+}
+
+func NewMerkleNode(left, right *MerkleNode, data []byte) *MerkleNode {
+	node := MerkleNode{}
+
+	if left == nil && right == nil {
+		hash := sha256.Sum256(data)
+		node.Data = hash[:]
+	} else {
+		prevHashes := append(left.Data, right.Data...)
+		hash := sha256.Sum256(prevHashes)
+		node.Data = hash[:]
+	}
+
+	node.Left = left
+	node.Right = right
+
+	return &node
 }
